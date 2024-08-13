0f10621b28600219e119eda04a5f5b2c1ed10abd
Chong <monkey.d.chong@gmail.com>
2024-08-14 02:01:00 +0700

    checkout

---
+++ app/Http/Controllers/QrPaymentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QrPaymentController extends Controller
+{
+    public function index() {
+        dd(123);
+    }
+}
